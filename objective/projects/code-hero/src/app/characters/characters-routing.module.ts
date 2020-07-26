65fd0152f0b9ecf28639c7e88bf7f060ccd26ffa
Ramon Conrado <ramonconrado94@gmail.com>
2020-07-26 16:52:11 -0300

    ajustes basicos de layout~

--- objective/projects/code-hero/src/app/characters/characters-routing.module.ts
+++ objective/projects/code-hero/src/app/characters/characters-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CharactersComponent } from './characters.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
 
-const routes: Routes = [{ path: '', component: CharactersComponent }];
+const routes: Routes = [
+  { path: '', component: CharactersComponent },
+  { path: ':id', component: CharacterDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
